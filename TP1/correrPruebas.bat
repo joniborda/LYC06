1e637587e69726812b9d60dab490cd12d3d95e1b
joniborda <jonathanmatiasborda@gmail.com>
2020-04-19 16:43:17 -0300

    Mas pruebas if y while

--- TP1/correrPruebas.bat
+++ TP1/correrPruebas.bat
@@ -1,10 +1,13 @@
 flex Lexico.l
 bison -dyv Sintactico.y
 gcc.exe lex.yy.c y.tab.c -o Primera.exe
-Primera.exe pruebas/identificadorContieneIf.txt
-Primera.exe pruebas/ifelse.txt
-Primera.exe pruebas/ifsimple.txt
-Primera.exe pruebas/sentenciasAnidadas.txt
+Primera.exe pruebas/if/identificadorContieneIf.txt
+Primera.exe pruebas/if/ifelse.txt
+Primera.exe pruebas/if/ifsimple.txt
+Primera.exe pruebas/if/sentenciasAnidadas.txt
+Primera.exe pruebas/while/whilesimple.txt
+Primera.exe pruebas/while/identificadorContieneWhile.txt
+Primera.exe pruebas/while/sentenciasAnidadas.txt
 del lex.yy.c
 del y.tab.c
 del y.output
