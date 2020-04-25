0384173e875d471246c6ce7fa858164c76f931ea
joniborda <jonathanmatiasborda@gmail.com>
2020-04-25 08:42:58 -0300

    Pruebas con y sin error

--- TP1/compilarPruebas.bat
+++ TP1/compilarPruebas.bat
@@ -5,7 +5,7 @@ bison -dyv Sintactico.y
 gcc.exe lex.yy.c y.tab.c archivos_punto_C/tabla_simbolo.c -o Primera.exe
 pause
 
-@cd pruebas
+@cd pruebas_sin_error
 @for %%x in (*) do @(
 	@echo --------------------- %%x ---------------------
 	
@@ -15,10 +15,31 @@ pause
 		  	@echo .
 		  	@echo /****************** ERROR ***************/
 		  	delete.bat
+		  	exit 0
 		)
 	)
 )
+
+@cd ../pruebas_con_error
+@for %%x in (*) do @(
+	@echo .
+	@echo --------------------- %%x ---------------------
+	
+	@if	 "ts.txt" NEQ "%%x" (
+		..\Primera.exe %%x noImprimir
+		..\Primera.exe %%x noImprimir && (
+		  	@echo .
+		  	@echo /***************** DEBERIA ARROJAR ERROR ***************/
+		  	@cd ..\
+			@delete.bat
+			@pause
+			exit 0
+
+		)
+	)
+)
+@echo .
+@echo /****************** PRUEBAS OK ***************/
 @cd ..\
 @delete.bat
-
 @pause
